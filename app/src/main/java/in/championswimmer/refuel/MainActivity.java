989ecbb919fd59127851ec5a6f3fe2265cbb9e52
Arnav Gupta <championswimmer@gmail.com>
2015-10-16 23:11:34 +0530

    initial commit
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ app/src/main/java/in/championswimmer/refuel/MainActivity.java
@@ -0,0 +1,63 @@
+package in.championswimmer.refuel;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get LayoutInflater and inflate the dialog view.
+                LayoutInflater li = LayoutInflater.from(getBaseContext());
+                View dialogView = li.inflate(R.layout.dialog_add_refuel_entry, null);
+
+                //Set up the dialog using an AlertDialogBuilder
+                AlertDialog.Builder adBuilder = new AlertDialog.Builder(MainActivity.this, R.style.AppTheme_AlertDialog);
+                adBuilder.setView(dialogView);
+
+                AlertDialog refuelDialog = adBuilder.create();
+                refuelDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

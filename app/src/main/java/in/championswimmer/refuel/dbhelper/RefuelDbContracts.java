52557da5311b81dcc2bfa3d86f219bad8e204f95
Arnav Gupta <championswimmer@gmail.com>
2015-10-18 07:09:39 +0530

    make initial db helper classes

---
+++ app/src/main/java/in/championswimmer/refuel/dbhelper/RefuelDbContracts.java
@@ -0,0 +1,34 @@
+package in.championswimmer.refuel.dbhelper;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by championswimmer on 18/10/15.
+ */
+public interface RefuelDbContracts {
+
+    String TEXT_TYPE = " TEXT";
+    String INTEGER_TYPE = " INTEGER";
+    String COMMA_SEP = ",";
+
+
+
+    interface RefuelEntry extends BaseColumns {
+        String TABLE_NAME = "refuel_entry";
+
+        String COL_FUEL_FILLED = "fuel_filled";
+        String COL_MONEY_PAID = "money_paid";
+        String COL_RATE_PER_LIT = "rate_per_lit";
+        String COL_DID_FULL_TANK = "did_full_tank";
+
+        String COMMAND_CREATE_ENTRIES =
+                "CREATE TABLE IF NOT EXISTS" + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY," +
+                        COL_FUEL_FILLED + TEXT_TYPE + COMMA_SEP +
+                        COL_MONEY_PAID + TEXT_TYPE + COMMA_SEP +
+                        COL_RATE_PER_LIT + TEXT_TYPE + COMMA_SEP +
+                        COL_DID_FULL_TANK + INTEGER_TYPE + COMMA_SEP +
+                " )";
+
+    }
+}
---
+++ app/src/main/java/in/championswimmer/refuel/dbhelper/RefuelDbHelper.java
@@ -0,0 +1,51 @@
+package in.championswimmer.refuel.dbhelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by championswimmer on 18/10/15.
+ */
+public class RefuelDbHelper extends SQLiteOpenHelper{
+
+    public static final int DB_VER = 1;
+    public static final String DB_NAME = "Refuel.db";
+
+    private static RefuelDbHelper refuelDbSingleton = null;
+
+    public static RefuelDbHelper getInstance(Context c) {
+        if (refuelDbSingleton == null ) {
+            refuelDbSingleton = new RefuelDbHelper(c.getApplicationContext());
+
+        }
+        return refuelDbSingleton;
+    }
+
+    public RefuelDbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(RefuelDbContracts.RefuelEntry.COMMAND_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public static boolean addNewRefuelEntry(Context ctx, float fuelAmt, float rate, float moneyAmt) {
+        SQLiteDatabase db = getInstance(ctx).getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(RefuelDbContracts.RefuelEntry.COL_FUEL_FILLED, String.valueOf(fuelAmt));
+        cv.put(RefuelDbContracts.RefuelEntry.COL_RATE_PER_LIT, String.valueOf(rate));
+        cv.put(RefuelDbContracts.RefuelEntry.COL_MONEY_PAID, String.valueOf(moneyAmt));
+
+        db.insert(RefuelDbContracts.RefuelEntry.TABLE_NAME, null, cv);
+        return true;
+    }
+}
